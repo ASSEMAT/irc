5704ca9adc4a8e02954bef24dd1e4a382f9e2d30
assemat <dominique.assemat@berger-levrault.fr>
2014-06-13 14:14:30 +0200

    Rajout de la gestion de l'avatar

--- FisrtIRCStarterWithUI/src/com/cfranc/irc/ui/FrameSignUser.java
+++ FisrtIRCStarterWithUI/src/com/cfranc/irc/ui/FrameSignUser.java
@@ -1,34 +1,30 @@
 package com.cfranc.irc.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
-import javax.swing.JButton;
-
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.GridBagLayout;
-
-import javax.swing.JTextArea;
-
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-
-import javax.swing.JLabel;
-import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 import com.cfranc.irc.server.UserGestion;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 
 public class FrameSignUser extends JFrame {
 
@@ -42,8 +38,9 @@ public class FrameSignUser extends JFrame {
 	private JLabel lblMotDePasse;
 	private JTextField signPassword;
 	private JLabel lblAvatar_1;
-	private JButton btnParcourir;
-	private JPanel panelPicture;
+//	private JPanel panelPicture;
+	private JTextField chemin;
+	private JImagePanel panelPicture ;
 
 	/**
 	 * Launch the application.
@@ -67,7 +64,7 @@ public class FrameSignUser extends JFrame {
 	public FrameSignUser() {
 		setPreferredSize(new Dimension(500, 500));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 450, 383);
 		contentPane = new JPanel();
 		contentPane.setPreferredSize(new Dimension(400, 500));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -90,7 +87,7 @@ public class FrameSignUser extends JFrame {
 		gbc_lblNewLabel.fill = GridBagConstraints.BOTH;
 		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 1;
-		gbc_lblNewLabel.gridy = 4;
+		gbc_lblNewLabel.gridy = 2;
 		panel1.add(lblNewLabel, gbc_lblNewLabel);
 		
 		signLogin = new JTextField();
@@ -98,7 +95,7 @@ public class FrameSignUser extends JFrame {
 		gbc_signLogin.insets = new Insets(0, 0, 5, 5);
 		gbc_signLogin.fill = GridBagConstraints.HORIZONTAL;
 		gbc_signLogin.gridx = 2;
-		gbc_signLogin.gridy = 4;
+		gbc_signLogin.gridy = 2;
 		panel1.add(signLogin, gbc_signLogin);
 		signLogin.setColumns(10);
 		
@@ -107,7 +104,7 @@ public class FrameSignUser extends JFrame {
 		gbc_lblPseudo.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPseudo.anchor = GridBagConstraints.WEST;
 		gbc_lblPseudo.gridx = 1;
-		gbc_lblPseudo.gridy = 5;
+		gbc_lblPseudo.gridy = 3;
 		panel1.add(lblPseudo, gbc_lblPseudo);
 		
 		signPseudo = new JTextField();
@@ -115,7 +112,7 @@ public class FrameSignUser extends JFrame {
 		gbc_signPseudo.insets = new Insets(0, 0, 5, 5);
 		gbc_signPseudo.fill = GridBagConstraints.HORIZONTAL;
 		gbc_signPseudo.gridx = 2;
-		gbc_signPseudo.gridy = 5;
+		gbc_signPseudo.gridy = 3;
 		panel1.add(signPseudo, gbc_signPseudo);
 		signPseudo.setColumns(10);
 		
@@ -124,7 +121,7 @@ public class FrameSignUser extends JFrame {
 		gbc_lblPrenom.anchor = GridBagConstraints.WEST;
 		gbc_lblPrenom.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPrenom.gridx = 1;
-		gbc_lblPrenom.gridy = 6;
+		gbc_lblPrenom.gridy = 4;
 		panel1.add(lblPrenom, gbc_lblPrenom);
 		
 		signPrenom = new JTextField();
@@ -132,7 +129,7 @@ public class FrameSignUser extends JFrame {
 		gbc_signPrenom.insets = new Insets(0, 0, 5, 5);
 		gbc_signPrenom.fill = GridBagConstraints.HORIZONTAL;
 		gbc_signPrenom.gridx = 2;
-		gbc_signPrenom.gridy = 6;
+		gbc_signPrenom.gridy = 4;
 		panel1.add(signPrenom, gbc_signPrenom);
 		signPrenom.setColumns(10);
 		
@@ -141,7 +138,7 @@ public class FrameSignUser extends JFrame {
 		gbc_lblMotDePasse.anchor = GridBagConstraints.WEST;
 		gbc_lblMotDePasse.insets = new Insets(0, 0, 5, 5);
 		gbc_lblMotDePasse.gridx = 1;
-		gbc_lblMotDePasse.gridy = 7;
+		gbc_lblMotDePasse.gridy = 5;
 		panel1.add(lblMotDePasse, gbc_lblMotDePasse);
 		
 		signPassword = new JTextField();
@@ -149,7 +146,7 @@ public class FrameSignUser extends JFrame {
 		gbc_signPassword.insets = new Insets(0, 0, 5, 5);
 		gbc_signPassword.fill = GridBagConstraints.HORIZONTAL;
 		gbc_signPassword.gridx = 2;
-		gbc_signPassword.gridy = 7;
+		gbc_signPassword.gridy = 5;
 		panel1.add(signPassword, gbc_signPassword);
 		signPassword.setColumns(10);
 		
@@ -158,25 +155,80 @@ public class FrameSignUser extends JFrame {
 		gbc_lblAvatar_1.anchor = GridBagConstraints.WEST;
 		gbc_lblAvatar_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblAvatar_1.gridx = 1;
-		gbc_lblAvatar_1.gridy = 9;
+		gbc_lblAvatar_1.gridy = 7;
 		panel1.add(lblAvatar_1, gbc_lblAvatar_1);
 		
-		panelPicture = new JPanel();
+		chemin = new JTextField();
+		GridBagConstraints gbc_chemin = new GridBagConstraints();
+		gbc_chemin.insets = new Insets(0, 0, 5, 5);
+		gbc_chemin.fill = GridBagConstraints.HORIZONTAL;
+		gbc_chemin.gridx = 2;
+		gbc_chemin.gridy = 7;
+		panel1.add(chemin, gbc_chemin);
+		chemin.setColumns(10);
+		
+		
+		panelPicture = new JImagePanel();
+		Dimension dim = new Dimension(200, 100);
+		panelPicture.setPreferredSize(dim);
+		panelPicture.setMinimumSize(dim);
+		panelPicture.setMaximumSize(dim);
+		panelPicture.setSize(dim);
 		panelPicture.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		
+		//Create a file chooser
+		final JFileChooser fc = new JFileChooser();
+		
+		panelPicture.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				 int returnVal = fc.showOpenDialog(chemin);
+			        if (returnVal == JFileChooser.APPROVE_OPTION) {
+			            File file = fc.getSelectedFile();
+			            //This is where a real application would open the file.
+			            chemin.setText(file.toString()); 
+			            System.out.println(chemin);
+			        	panelPicture.setImage(new ImageIcon(chemin.getText()).getImage());
+			    		repaint();
+			        }
+			   } 	
+			});
+			
+		
+		
 		GridBagConstraints gbc_panelPicture = new GridBagConstraints();
 		gbc_panelPicture.gridheight = 4;
-		gbc_panelPicture.insets = new Insets(0, 0, 5, 5);
+		gbc_panelPicture.insets = new Insets(0, 0, 0, 5);
 		gbc_panelPicture.fill = GridBagConstraints.BOTH;
 		gbc_panelPicture.gridx = 2;
 		gbc_panelPicture.gridy = 9;
 		panel1.add(panelPicture, gbc_panelPicture);
-		
-		btnParcourir = new JButton("Parcourir");
-		GridBagConstraints gbc_btnParcourir = new GridBagConstraints();
-		gbc_btnParcourir.insets = new Insets(0, 0, 5, 0);
-		gbc_btnParcourir.gridx = 3;
-		gbc_btnParcourir.gridy = 9;
-		panel1.add(btnParcourir, gbc_btnParcourir);
 		
 		JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.SOUTH);
